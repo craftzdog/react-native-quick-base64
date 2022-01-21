cab9c8950bdc4bbc1f36a690742c207a875bdeef
Takuya Matsuyama <hi@craftz.dog>
2022-01-21 15:01:03 +0900

    fix(both): fallback to js impl. when on remote debugger

--- src/index.tsx
+++ src/index.tsx
@@ -1,67 +1,85 @@
-declare function base64ToArrayBuffer(data: string): ArrayBuffer;
-declare function base64FromArrayBuffer(data: string | ArrayBuffer): string;
+import fallback from 'base64-js'
+
+type FuncBase64ToArrayBuffer = (data: string) => ArrayBuffer
+type FuncBase64FromArrayBuffer = (data: string | ArrayBuffer) => string
+
+declare var base64ToArrayBuffer: FuncBase64ToArrayBuffer | undefined
+declare const base64FromArrayBuffer: FuncBase64FromArrayBuffer | undefined
 
 // from https://github.com/beatgammit/base64-js/blob/master/index.js
 function getLens(b64: string) {
-  let len = b64.length;
+  let len = b64.length
 
   if (len % 4 > 0) {
-    throw new Error('Invalid string. Length must be a multiple of 4');
+    throw new Error('Invalid string. Length must be a multiple of 4')
   }
 
-  let validLen = b64.indexOf('=');
-  if (validLen === -1) validLen = len;
+  let validLen = b64.indexOf('=')
+  if (validLen === -1) validLen = len
 
-  let placeHoldersLen = validLen === len ? 0 : 4 - (validLen % 4);
+  let placeHoldersLen = validLen === len ? 0 : 4 - (validLen % 4)
 
-  return [validLen, placeHoldersLen];
+  return [validLen, placeHoldersLen]
 }
 
 function uint8ArrayToString(array: Uint8Array) {
   let out = '',
     i = 0,
-    len = array.length;
+    len = array.length
   while (i < len) {
-    const c = array[i++];
-    out += String.fromCharCode(c);
+    const c = array[i++]
+    out += String.fromCharCode(c)
   }
-  return out;
+  return out
 }
 
 function stringToArrayBuffer(str: string) {
-  const buf = new ArrayBuffer(str.length);
-  const bufView = new Uint8Array(buf);
+  const buf = new ArrayBuffer(str.length)
+  const bufView = new Uint8Array(buf)
   for (let i = 0, strLen = str.length; i < strLen; i++) {
-    bufView[i] = str.charCodeAt(i);
+    bufView[i] = str.charCodeAt(i)
   }
-  return buf;
+  return buf
 }
 
 export function byteLength(b64: string): number {
-  let lens = getLens(b64);
-  let validLen = lens[0];
-  let placeHoldersLen = lens[1];
-  return ((validLen + placeHoldersLen) * 3) / 4 - placeHoldersLen;
+  let lens = getLens(b64)
+  let validLen = lens[0]
+  let placeHoldersLen = lens[1]
+  return ((validLen + placeHoldersLen) * 3) / 4 - placeHoldersLen
 }
 
 export function toByteArray(b64: string): Uint8Array {
-  return new Uint8Array(base64ToArrayBuffer(b64));
+  if (typeof base64ToArrayBuffer !== 'undefined') {
+    return new Uint8Array(base64ToArrayBuffer(b64))
+  } else {
+    return fallback.toByteArray(b64)
+  }
 }
 
 export function fromByteArray(uint8: Uint8Array): string {
-  return base64FromArrayBuffer(uint8.buffer);
+  if (typeof base64FromArrayBuffer !== 'undefined') {
+    return base64FromArrayBuffer(uint8.buffer)
+  } else {
+    return fallback.fromByteArray(uint8)
+  }
 }
 
 export function btoa(data: string): string {
-  return base64FromArrayBuffer(stringToArrayBuffer(data));
+  const ab = stringToArrayBuffer(data)
+  if (typeof base64FromArrayBuffer !== 'undefined') {
+    return base64FromArrayBuffer(ab)
+  } else {
+    return fallback.fromByteArray(new Uint8Array(ab))
+  }
 }
 
 export function atob(b64: string): string {
-  const ua = new Uint8Array(base64ToArrayBuffer(b64));
-  return uint8ArrayToString(ua);
+  const ua = toByteArray(b64)
+  return uint8ArrayToString(ua)
 }
 
 export function shim() {
-  (global as any).btoa = btoa;
-  (global as any).atob = atob;
+  ;(global as any).btoa = btoa
+  ;(global as any).atob = atob
 }
