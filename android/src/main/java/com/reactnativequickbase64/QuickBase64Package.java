d8c292b4504f849ac8011368e85660e101df45da
Takuya Matsuyama <hi@craftz.dog>
2022-01-21 15:39:38 +0900

    feat(android): autoinstall
    It automatically initializes the native module on Android while avoiding the race condition crash
    issue of #2
    
    BREAKING CHANGE: Remove the manual installation from your Android app

--- android/src/main/java/com/reactnativequickbase64/QuickBase64Package.java
+++ android/src/main/java/com/reactnativequickbase64/QuickBase64Package.java
@@ -7,7 +7,6 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -16,7 +15,7 @@ public class QuickBase64Package implements ReactPackage {
   @NonNull
   @Override
   public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
-    return Arrays.<NativeModule>asList(new QuickBase64Module(reactContext));
+    return Collections.singletonList(new QuickBase64Module(reactContext));
   }
 
   @NonNull
