3dbd5fa795557d95ae02c10b6d445a4972160767
Hirbod Mirjavadi <hm@nightstomp.com>
2025-05-27 14:11:17 +0200

    fix: metro.config.js

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,40 +1,51 @@
-const path = require('path');
-const blacklist = require('metro-config/src/defaults/exclusionList');
-const escape = require('escape-string-regexp');
-const pak = require('../package.json');
+const path = require("path");
 
-const root = path.resolve(__dirname, '..');
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+const escape = require("escape-string-regexp");
+const exclusionList = require("metro-config/src/defaults/exclusionList");
 
-const modules = Object.keys({
-  ...pak.peerDependencies,
-});
+const pack = require("../package.json");
 
-module.exports = {
+const root = path.resolve(__dirname, "..");
+
+const modules = Object.keys(pack.peerDependencies);
+
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('@react-native/metro-config').MetroConfig}
+ */
+const config = {
   projectRoot: __dirname,
   watchFolders: [root],
 
   // We need to make sure that only one version is loaded for peerDependencies
-  // So we blacklist them at the root, and alias them to the versions in example's node_modules
+  // So we exclude them at the root, and alias them to the versions in example's node_modules
   resolver: {
-    blacklistRE: blacklist(
+    blacklistRE: exclusionList(
       modules.map(
         (m) =>
-          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
-      )
+          new RegExp(`^${escape(path.join(root, "node_modules", m))}\\/.*$`),
+      ),
     ),
 
-    extraNodeModules: modules.reduce((acc, name) => {
-      acc[name] = path.join(__dirname, 'node_modules', name);
-      return acc;
-    }, {}),
-  },
+    extraNodeModules: modules.reduce(
+      (acc, name) => {
+        acc[name] = path.join(__dirname, "node_modules", name);
 
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: true,
-        inlineRequires: true,
+        return acc;
       },
-    }),
+      {
+        // required from RN 0.73
+        "react-native-quick-base64": path.join(
+          __dirname,
+          "node_modules",
+          "react-native-quick-base64",
+        ),
+      },
+    ),
   },
 };
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
