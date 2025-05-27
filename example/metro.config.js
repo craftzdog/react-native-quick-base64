9761aa889db330ec394fdca76a217b1658608daf
Hirbod Mirjavadi <hm@nightstomp.com>
2025-05-27 12:47:21 +0200

    chore(tsconfig.json, example): update TypeScript configuration and modify Metro config to use dynamic import; bump Node engine requirement to 20.19.0

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,16 +1,14 @@
 const path = require('path')
 const { getDefaultConfig } = require('@react-native/metro-config')
-const { withMetroConfig } = require('react-native-monorepo-config')
 
-const root = path.resolve(__dirname, '..')
+async function getMetroConfig() {
+  const { withMetroConfig } = await import('react-native-monorepo-config')
+  const root = path.resolve(__dirname, '..')
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-module.exports = withMetroConfig(getDefaultConfig(__dirname), {
-  root,
-  dirname: __dirname
-})
+  return withMetroConfig(getDefaultConfig(__dirname), {
+    root,
+    dirname: __dirname
+  })
+}
+
+module.exports = (async () => await getMetroConfig())()
