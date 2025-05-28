0952a1659fd74eb0a957e6a71f697dafb4debe0a
Takuya Matsuyama <hi@craftz.dog>
2025-05-28 15:14:00 +0900

    style(eslint): make linter happy for metro config

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,14 +1,14 @@
-const path = require("path");
+const path = require('path')
 
-const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
-const escape = require("escape-string-regexp");
-const exclusionList = require("metro-config/src/defaults/exclusionList");
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
+const escape = require('escape-string-regexp')
+const exclusionList = require('metro-config/src/defaults/exclusionList')
 
-const pack = require("../package.json");
+const pack = require('../package.json')
 
-const root = path.resolve(__dirname, "..");
+const root = path.resolve(__dirname, '..')
 
-const modules = Object.keys(pack.peerDependencies);
+const modules = Object.keys(pack.peerDependencies)
 
 /**
  * Metro configuration
@@ -25,27 +25,26 @@ const config = {
   resolver: {
     blacklistRE: exclusionList(
       modules.map(
-        (m) =>
-          new RegExp(`^${escape(path.join(root, "node_modules", m))}\\/.*$`),
-      ),
+        m => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
+      )
     ),
 
     extraNodeModules: modules.reduce(
       (acc, name) => {
-        acc[name] = path.join(__dirname, "node_modules", name);
+        acc[name] = path.join(__dirname, 'node_modules', name)
 
-        return acc;
+        return acc
       },
       {
         // required from RN 0.73
-        "react-native-quick-base64": path.join(
+        'react-native-quick-base64': path.join(
           __dirname,
-          "node_modules",
-          "react-native-quick-base64",
-        ),
-      },
-    ),
-  },
-};
+          'node_modules',
+          'react-native-quick-base64'
+        )
+      }
+    )
+  }
+}
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(getDefaultConfig(__dirname), config)
